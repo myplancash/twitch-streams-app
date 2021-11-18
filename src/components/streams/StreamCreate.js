bbd534861404e6a2a189f0dd0502607708073f4d
myplancash <myplancash@gmail.com>
2021-11-18 16:08:42 -0500

    Added a Form using reduxForm and Field to render some inputs over a StreamCreate Route

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,9 +1,46 @@
+import { getDefaultNormalizer } from '@testing-library/dom';
 import React from 'react';
+import {Field, reduxForm} from 'redux-form'
 
-const StreamCreate = () => {
-  return (
-    <div>StreamCreate</div>
-  );
+class StreamCreate extends React.Component {
+
+  renderInput({input, label}) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input value={input.value} onChange={input.onChange} {...input} />
+      </div>
+    );
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className='ui form'>
+        <Field name="title" component={this.renderInput} label="Title Label" />
+        <Field name="description" component={this.renderInput} label="Description Label"/>
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
 }
 
-export default StreamCreate;
+const validate = (formValues) => {
+  const errors = {}
+
+  if (!formValues.title) {
+    errors.title = 'You must enter a title'
+  }
+
+  if (!formValues.description) {
+    errors.description = 'You must enter a description'
+  }
+  return errors;
+}
+
+export default reduxForm({
+  form: 'streamCreate'
+})(StreamCreate);
--- src/index.js
+++ src/index.js
@@ -7,11 +7,16 @@ import {createStore, applyMiddleware, compose} from 'redux'
 import App from './components/App';
 import reducers from './reducers'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 }) || compose;
 const store = createStore(
   reducers,
   composeEnhancers(applyMiddleware())
 )
+
+// const store = createStore(reducer, preloadedState, composeEnhancers(
+//   applyMiddleware(invariant(), thunk)
+// ));
 
 
 ReactDOM.render(
